ba713ba9c0ae68e0646af62a17ef0ab1e3dc7386
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-15 19:29:54 +0530

    Add queue management system

---
+++ 03-queue-mgmt-system/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        <header className="text-center pb-30">
+          <h1 className="text-4xl my-4 text-purple-500 font-medium">Queue Maangement Application</h1>
+          <p className="text-sm text-gray-400">Manage your customers efficiently</p>
+        </header>
+
+        <main className="display-flex justify-between items-center px-10 ">
+
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ 03-queue-mgmt-system/src/components/Form.jsx
@@ -0,0 +1,12 @@
+export default function Form() {
+    return (
+        <>
+            <div>
+                <h2>Add a customer</h2>
+                <div>
+                    
+                </div>
+            </div>
+        </>
+    )
+}

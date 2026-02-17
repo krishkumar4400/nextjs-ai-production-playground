e9ef67382c3f43a29e4e0408751f01dc6544e58c
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-17 23:10:58 +0530

    project complete

--- 03-queue-mgmt-system/src/App.jsx
+++ 03-queue-mgmt-system/src/App.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import Display from "./components/Display";
+
 export default function App() {
+  const [queue, setQueue] = useState([]);
+
+  const addToQueue = (customer) => {
+    // add data to queue
+    setQueue([...queue, {...customer, id: Date.now(), status: "Waiting"}]);
+  }
+
+  const updateStatus = (id, newStatus) => {
+    // change data in queue
+    setQueue(queue.map(customer => {
+      customer.id === id ? {...customer, status: newStatus} : customer
+    }));
+  }
+  
+  const removeFromQueue = (id) => {
+    // remove data from the queue
+    setQueue(queue.filter(customer => customer.id !== id));
+  }
+   
   return (
     <>
       <div>
@@ -7,8 +30,9 @@ export default function App() {
           <p className="text-sm text-gray-400">Manage your customers efficiently</p>
         </header>
 
-        <main className="display-flex justify-between items-center px-10 ">
-
+        <main className="flex justify-between items-center flex-wrap gap-4 px-10 ">
+          <Form onAdd={addToQueue} />
+          <Display queue={queue} onUpdateStatus={updateStatus} onRemoveFromQueue={removeFromQueue} />
         </main>
       </div>
     </>

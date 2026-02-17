a50927dd295668db2f685c6f7bbfaed14537c90c
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-02-17 23:11:57 +0530

    project complete

--- 03-queue-mgmt-system/src/App.jsx
+++ 03-queue-mgmt-system/src/App.jsx
@@ -7,32 +7,42 @@ export default function App() {
 
   const addToQueue = (customer) => {
     // add data to queue
-    setQueue([...queue, {...customer, id: Date.now(), status: "Waiting"}]);
-  }
+    setQueue([...queue, { ...customer, id: Date.now(), status: "Waiting" }]);
+  };
 
   const updateStatus = (id, newStatus) => {
     // change data in queue
-    setQueue(queue.map(customer => {
-      customer.id === id ? {...customer, status: newStatus} : customer
-    }));
-  }
-  
+    setQueue(
+      queue.map((customer) => {
+        customer.id === id ? { ...customer, status: newStatus } : customer;
+      }),
+    );
+  };
+
   const removeFromQueue = (id) => {
     // remove data from the queue
-    setQueue(queue.filter(customer => customer.id !== id));
-  }
-   
+    setQueue(queue.filter((customer) => customer.id !== id));
+  };
+
   return (
     <>
       <div>
         <header className="text-center pb-30">
-          <h1 className="text-4xl my-4 text-purple-500 font-medium">Queue Maangement Application</h1>
-          <p className="text-sm text-gray-400">Manage your customers efficiently</p>
+          <h1 className="text-4xl my-4 text-purple-500 font-medium">
+            Queue Maangement Application
+          </h1>
+          <p className="text-sm text-gray-400">
+            Manage your customers efficiently
+          </p>
         </header>
 
         <main className="flex justify-between items-center flex-wrap gap-4 px-10 ">
           <Form onAdd={addToQueue} />
-          <Display queue={queue} onUpdateStatus={updateStatus} onRemoveFromQueue={removeFromQueue} />
+          <Display
+            queue={queue}
+            onUpdateStatus={updateStatus}
+            onRemoveFromQueue={removeFromQueue}
+          />
         </main>
       </div>
     </>
